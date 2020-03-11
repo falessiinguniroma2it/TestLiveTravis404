86a416e86fc685b4cd0bbfc9453aeb88b2b176b9
falessiinguniroma2it <56582812+falessiinguniroma2it@users.noreply.github.com>
2020-03-11 16:14:16 +0100

    Prova travis

---
+++ src/Logic/Hello2.java
@@ -0,0 +1,10 @@
+package Logic;
+
+public class Hello2 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World!!!");
+    }
+
+}
